672391fafaa0ed897717afbd23182ba8ba2d5cd6
Amir Aiman <amiraiman.jj@gmail.com>
2026-05-20 08:58:07 +0800

    sample map

--- main.go
+++ main.go
@@ -4,10 +4,36 @@ import (
 	"fmt"
 )
 
-func main() {
-	colors := map[string]string{
-		"red": "#ff0000",
+func getColor(m map[string]string, k string) string {
+	val, ok := m[k]
+	if !ok {
+		return "No colors found"
 	}
 
-	fmt.Println(colors)
+	return val
+}
+
+func describe(m map[string]string) {
+	fmt.Println("ALL COLORS:")
+	for key, value := range m {
+		fmt.Printf("%v: %v\n", key, value)
+	}
+}
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"black": "#000",
+		"white": "#fff",
+	}
+	colors["blue"] = "#0000ff"
+
+	describe(colors)
+	fmt.Println("FOUND COLOR:", getColor(colors, "blue"))
+
+	delete(colors, "blue")
+
+	describe(colors)
+	fmt.Println("FOUND COLOR:", getColor(colors, "blue"))
 }
